209e9e7508f3ff4f4a54953386bbcda0373c1377
May Tusek <tusek.may@gmail.com>
2023-05-28 01:30:20 -0700

    Begins work on authentication service.

--- projects/backend/src/app.module.ts
+++ projects/backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common"
 import { UserController } from "./user/user.controller"
 import { UserService } from "./user/user.service"
 import { PrismaService } from "./prisma.service"
+import { AuthController } from "./auth/auth.controller"
+import { AuthService } from "./auth/auth.service"
 
 @Module({
   imports: [],
-  controllers: [UserController],
-  providers: [PrismaService, UserService],
+  controllers: [UserController, AuthController],
+  providers: [PrismaService, UserService, AuthService],
 })
 export class AppModule {}
