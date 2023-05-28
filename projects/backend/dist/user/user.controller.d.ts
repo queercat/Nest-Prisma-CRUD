fd7fdb737960872799f8f029fc58b561a014a8b1
May Tusek <tusek.may@gmail.com>
2023-05-28 01:30:49 -0700

    Removes dist.

--- projects/backend/dist/user/user.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserService } from "./user.service";
-import { Prisma } from "@prisma/client";
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    createUser(body: Prisma.UserCreateInput, res: any): Promise<void>;
-}
--- projects/backend/dist/user/user.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from "../prisma.service";
-import { Prisma, User } from "@prisma/client";
-export declare class UserService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    createUser(data: Prisma.UserCreateInput): Promise<User>;
-}
